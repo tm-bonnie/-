cd3d96e776f61863dbd6bf223c65c7698c1f5cbb
tm-bonnie <bonnietimur9@icloud.com>
2025-05-24 00:34:13 +0500

    Add files via upload

---
+++ работа/server.js
@@ -0,0 +1,16 @@
+// Установи ws: npm install ws
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    // Рассылаем сообщение всем клиентам
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
+
+console.log('WebSocket сервер запущен на порту 8080');
